5cf29ed2e2e4f899ebb684c16ab65ce0fc5dd3d8
juarezCynthia3 <52721954+juarezCynthia3@users.noreply.github.com>
2019-08-10 23:57:23 -0500

    second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,59 @@
+import os
+import csv
 
+csvpath = os.path.join('..','PyBank', 'budget_data.csv')
+
+total_months = 0
+total_profit_losses = 0
+value = 0
+change = 0
+months = []
+profits_losses = []
+
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csv_header = next(csvreader)
+    first_row = next(csvreader)
+    total_months += 1
+    total_profit_losses += int(first_row[1])
+    value = int(first_row[1])
+    
+#The total number of months included in the dataset
+    for row in csvreader:
+        months.append(row[0])
+        change = int(row[1]) - value
+        profits_losses.append(change)
+        value = int(row[1])
+        total_months += 1
+        
+#The net total amount of "Profit/Losses" over the entire period
+        total_profit_losses = total_profit_losses + int(row[1])
+
+#The average of the changes in "Profit/Losses" over the entire period
+average = sum(profits_losses)/len(profits_losses)
+
+#The greatest increase in profits (date and amount) over the entire period
+max_increase = max(profits_losses)
+max_index = profits_losses.index(max_increase)
+max_month = months[max_index]
+
+#Print Financial Analysis
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total_profit_losses)}")
+print(f"Average Change: ${str(round(average,2))}")
+print(f"Greatest Increase in Profits: {max_month} (${str(max_increase)})")
+print(f"Greatest Decrease in Profits: {min_month} (${str(min_increase)})")
+
+#txt file
+output = open("output.txt", "w")
+
+line1 = "Financial Analysis"
+line2 = "----------------------------"
+line3 = str(f"Total Months: {str(total_months)}")
+line4 = str(f"Total: ${str(total_profit_losses)}")
+line5 = str(f"Average Change: ${str(round(average,2))}")
+line6 = str(f"Greatest Increase in Profits: {max_month} (${str(max_increase)})")
+line7 = str(f"Greatest Decrease in Profits:  {min_month} (${str(min_increase)})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,63 @@
+import os
+import csv
+
+csvpath = os.path.join("election_data.csv")
+amt_votes = []
+candidates = []
+percent_votes = []
+total_votes = 0
+
+#The total number of votes cast
+with open(csvpath, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        total_votes += 1
+        
+#A complete list of candidates who received votes   
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            amt_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            amt_votes[index] += 1
+    
+#The percentage of votes each candidate won
+    for votes in amt_votes:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
+    
+#The winner of the election based on popular vote.
+    winner = max(amt_votes)
+    index = amt_votes.index(winner)
+    winning_candidate = candidates[index]
+
+# Print Analysis
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(amt_votes[i])})")
+print("-------------------------")
+print(f"Winner: {winning_candidate}")
+print("-------------------------")
+
+#Text Output
+output = open("output.txt", "w")
+line1 = "Election Results"
+line2 = "--------------------------"
+line3 = str(f"Total Votes: {str(total_votes)}")
+line4 = str("--------------------------")
+output.write('{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4))
+for i in range(len(candidates)):
+    line = str(f"{candidates[i]}: {str(percent_votes[i])} ({str(amt_votes[i])})")
+    output.write('{}\n'.format(line))
+line5 = "--------------------------"
+line6 = str(f"Winner: {winning_candidate}")
+line7 = "--------------------------"
 
